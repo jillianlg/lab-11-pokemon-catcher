21fcaf441ff4024705240389c4881efee31742a0
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-12 17:00:31 -0700

    add branch, add import data and grab DOM elements in app.js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 // import functions and grab DOM elements
+import { rawPokemonData } from './pokemon-data.js';
 
+const images = document.querySelector('lable > img');
+const radios = document.querySelectorAll('input');
+const caughtDiv = document.querySelector('#caught-div');
+const captured = document.querySelector('#captured-pokemon');
+const moreButton = document.querySelector('button');
+
+console.log(images);
+console.log(radios);
+console.log(caughtDiv);
+console.log(captured);
+console.log(moreButton);
 // initialize state
 
 // set event listeners to update state and DOM
