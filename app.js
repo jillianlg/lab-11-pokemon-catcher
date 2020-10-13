f390d82b3a4c2d16c697e0f566c5bb10dee99a5a
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-12 17:22:20 -0700

    add getRandomPokemon function, test in browser

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 // import functions and grab DOM elements
 import { rawPokemonData } from './pokemon-data.js';
 
-const images = document.querySelector('lable > img');
+const images = document.querySelectorAll('label > img');
 const radios = document.querySelectorAll('input');
 const caughtDiv = document.querySelector('#caught-div');
 const captured = document.querySelector('#captured-pokemon');
 const moreButton = document.querySelector('button');
 
-console.log(images);
-console.log(radios);
-console.log(caughtDiv);
-console.log(captured);
-console.log(moreButton);
 // initialize state
+let capturedPokemon = 0;
+
+function getRandomPokemon(someArray) {
+    const index = Math.floor(Math.random() * someArray.length);
+
+    return someArray[index];
+}
+
+let firstPokemon = getRandomPokemon(rawPokemonData);
+let secondPokemon = getRandomPokemon(rawPokemonData);
+let thirdPokemon = getRandomPokemon(rawPokemonData);
+
+while (firstPokemon.id === secondPokemon.id) {
+    firstPokemon = getRandomPokemon(rawPokemonData);
+}
+while (secondPokemon.id === thirdPokemon.id) {
+    secondPokemon = getRandomPokemon(rawPokemonData);
+}
+while (thirdPokemon.id === firstPokemon.id) {
+    thirdPokemon = getRandomPokemon(rawPokemonData);
+}
+
+radios[0].value = firstPokemon.id;
+images[0].src = firstPokemon.url_image;
+
+radios[1].value = secondPokemon.id;
+images[1].src = secondPokemon.url_image;
+
+radios[2].value = thirdPokemon.id;
+images[2].src = thirdPokemon.url_image;
 
 // set event listeners to update state and DOM
