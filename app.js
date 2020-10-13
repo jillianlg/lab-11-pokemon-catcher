f083e9618f0b92ff8d353f8ac0c103fc926f183e
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-13 12:37:07 -0700

    add encountered pokemon and captured pokemon tracker

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ const moreButton = document.querySelector('button');
 
 // initialize state
 let captures = 10;
-let capturedPokemon = 0;
-let encounteredPokemon = 0;
-let capturedPokemonArray = [];
-let encounteredPokemonArray = [];
+let pokemonResults = [];
 
 
 
@@ -23,7 +20,7 @@ function getRandomPokemon(someArray) {
 }
 
 
-// Render Random Pokemon
+// Render Random Pokemon Fuction - loops through raw data to update DOM
 function renderRandomPokemon() {
 
     let firstPokemon = getRandomPokemon(rawPokemonData);
@@ -40,41 +37,76 @@ function renderRandomPokemon() {
         thirdPokemon = getRandomPokemon(rawPokemonData);
     }
     
-    radios[0].value = firstPokemon.id;
+    radios[0].value = firstPokemon.pokemon;
     images[0].src = firstPokemon.url_image;
     
-    radios[1].value = secondPokemon.id;
+    radios[1].value = secondPokemon.pokemon;
     images[1].src = secondPokemon.url_image;
     
-    radios[2].value = thirdPokemon.id;
+    radios[2].value = thirdPokemon.pokemon;
     images[2].src = thirdPokemon.url_image;
 
+    // encounteredPokemonArray.push(firstPokemon.id, secondPokemon.id, thirdPokemon.id);
 }
-
 renderRandomPokemon();
 
-// set event listeners to update state and DOM
 
-moreButton.addEventListener('click', () => {
-    capturedPokemon++;
-      console.log(capturedPokemon);
-    encounteredPokemon++;
-      console.log(encounteredPokemon);
-    encounteredPokemon++;
-      console.log(encounteredPokemon);
-    encounteredPokemon++;
-      console.log(encounteredPokemon);
-    renderRandomPokemon();
-  });
-  
-  // change to FIND BY ID
-  // function removeById(someId) {
-    //   for (let i = 0; i < remainingTrees.length; i++) {
-      //       if (someId === remainingTrees[i].id) {
-        //           remainingTrees.splice(i, 1);
-        //       }    
-        //   }
-        // }
+// set event listeners to update state and DOM
+for (let i = 0; i < radios.length; i++) {
+    radios[i].addEventListener('change', (e) => {	
+      
+        captures--;
+        caughtDiv.classList.remove('hidden');
+
+        radios.forEach((radio) => {
+            let encounteredPokemon = findByName(pokemonResults, radio.value);
+            if (!encounteredPokemon) {
+                encounteredPokemon = {
+                    pokeName: radio.value,
+                    encountered: 1, 
+                    captured: 0},
+                pokemonResults.push(encounteredPokemon);
+            } else {
+                encounteredPokemon++;
+            }
+            
+        });
+        // console.log(pokemonResults, 'This is the one');
+
+        for (let i = 0; i < radios.length; i++) {
+            radios[i].disabled = true;
+            images[i].style.opacity = .5;
+        }
+
+        const playerSelectedPokemon = e.target.value;
+        console.log(pokemonResults, e.target.value);
+      
+
+        let capturedPokemon = findByName(pokemonResults, e.target.value);
+        console.log(capturedPokemon);
+        capturedPokemon.capture++;
+
+
+        renderRandomPokemon();
+
+    });
+}
+// Find Captured Pokemon By ID function to push into capturedPokemonArray for results
+function findByName(pokemonResults, pokemonName) {
+
+    for (let i = 0; i < pokemonResults.length; i++) {
+        const capturedPokemon = pokemonResults[i];
+
+        console.log(capturedPokemon, 'are you working');
+    
+        if (capturedPokemon.pokemon === pokemonName) {
+            return capturedPokemon.pokemon;
+        }
+    }
+}
+
+// Find Encountered Pokemon By ID function to push into capturedPokemonArray for results
+
 //encounteredPokemonArray.push(firstPokemon.id, secondPokemon.id, thirdPokemon.id);
 
 // function showResults() {
@@ -82,50 +114,30 @@ moreButton.addEventListener('click', () => {
 //     how to I triger the results page
 //       alert('done!');
 //   }
+// };
 
-//   caughtDiv.classList.add('hidden');
 
-//   for (let i = 0; i < radios.length; i++) {
-//       radios[i].disabled = false;
-//       radios[i].checked = false;
-//       images[i].style.opacity = 1;
-//   } 
+//no longer need this:
+// moreButton.addEventListener('click', () => {
+//     capturedPokemon++;
+//       console.log(capturedPokemon);
+//     // How do I tick up x3 encountered
+//       encounteredPokemon++;
+//       console.log(encounteredPokemon);
+//     renderRandomPokemon();
+//   });
 
-  // for (let i = 0; i < radios.length; i++) {
-  //   radios[i].addEventListener('change', (e) => {	
-  //     captures--;
+       // Results Array
+        // pokemonResults.push(
+        //     { pokeName: firstPokemon.pokemon, 
+        //         encountered: 1, 
+        //         captured: 0},
 
-  //     const playerCapturedPokemon(e.target.value);
-  //     console.log(playerCapturedPokemon);
-  //     capturedPokemonArray.push(playerCapturedPokemon);
-  //     console.log(capturedPokemonArray);
-  //   }
-    
-  // };
+        //     { pokeName: secondPokemon.pokemon, 
+        //         encountered: 1, 
+        //         captured: 0},
 
-      // for (let i = 0; i < radios.length; i++) {
-      //     radios[i].disabled = true;
-      //     images[i].style.opacity = .5;
-      // } 
-
-      // const playerCapturedPokemon = e.target.value === capturedPokemon.id;
-      
-
-      // console.log(playerCapturedPokemon);
-
-      // if (playerCapturedPokemon) {
-      //   //I I am not removing anything what is this function doing?
-      //   removeById(e.target.value);
-      //   } else {
-      //     capturedPokemon.push(e.target.value);
-      //     // needs a funcution to call onLoad and onClick
-      //     encounteredPokemonArray.push(firstPokemon.id, secondPokemon.id, thirdPokemon.id);
-      //   }
-
-          
-    //     capturedPokemon++;
-    //     encounteredPokemon++;
-    //     caughtDiv.classList.toggle('hidden');
-  
-  
-    // }
+        //     { pokeName: thirdPokemon.pokemon, 
+        //         encountered: 1, 
+        //         captured: 0}
+        // );
