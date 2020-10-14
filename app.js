c37504811676c1b430f92621109f0c49676e355e
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-14 16:10:22 -0700

    fixx css, clean up code, still a little buggy on random pokemon reload

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 // import functions and grab DOM elements
 import { rawPokemonData } from './pokemon-data.js';
 import { getRandomPokemon, setInLocalStorage, incrementEncounters, incrementCaptures } from './pokemon-utils.js';
-// import { images, radios, caughtDiv, moreButton } from './pokemon-const.js';
 
 const images = document.querySelectorAll('label > img');
 const radios = document.querySelectorAll('input');
 const caughtDiv = document.querySelector('#caught-div');
 const moreButton = document.querySelector('button');
-//Add back it if you get a chance to render pokemon neam on UX
-// const captured = document.querySelector('#captured-pokemon');
 
 // initialize state
 let captures = 10;
@@ -41,31 +38,17 @@ export function renderRandomPokemon() {
 }
 renderRandomPokemon();
 
-//  increment encountered
-
-// for (let i = 0; i < radios.length; i++) {
-//     const item = radios[i];
-//     incrementEncounters(pokemonResults, item.value);
-// }
-
 // set event listeners to update state and DOM
 for (let i = 0; i < radios.length; i++) {
     radios[i].addEventListener('change', (e) => {	
-       incrementEncounters(pokemonResults, radios[0].value);
-       incrementEncounters(pokemonResults, radios[1].value);
-       incrementEncounters(pokemonResults, radios[2].value);
-    //    console.log(myInc);
-       console.log(pokemonResults);
-    //    console.log(radios[i].value);
-
-    //    console.log(radios[0]);
-    //    console.log(radios[1]);
-    //    console.log(radios[2]);
-       caughtDiv.classList.remove('hidden');
-       radios[i].disabled = true;
+        caughtDiv.classList.remove('hidden');
+        radios[i].disabled = true;
         images[i].style.opacity = .5;
+
+        incrementEncounters(pokemonResults, radios[0].value);
+        incrementEncounters(pokemonResults, radios[1].value);
+        incrementEncounters(pokemonResults, radios[2].value);
         incrementCaptures(pokemonResults, e.target.value);
-        console.log(pokemonResults);
 
         setInLocalStorage('RESULTS', pokemonResults);
     });
@@ -73,13 +56,12 @@ for (let i = 0; i < radios.length; i++) {
 
 moreButton.addEventListener('click', () => {
     captures--;
-    // console.log(captures);
     for (let i = 0; i < radios.length; i++) {
         radios[i].disabled = false;
         images[i].style.opacity = 100;
     }
     if (captures === 0) {
-        // radios[i].disabled = true;
+
         window.location.href = './results/index.html';
     }
     renderRandomPokemon();
